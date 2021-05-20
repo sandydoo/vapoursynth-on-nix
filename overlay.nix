0e95e8eecaa54cdb4ceb8256cb29dbe593922d51
sandydoo <hey@sandydoo.me>
2021-05-20 23:15:16 +0300

    Fix overlay imports

--- overlay.nix
+++ overlay.nix
@@ -1,9 +1,9 @@
 self: super: rec {
-  lsmash = import ../lsmash.nix;
-  vapoursynth-lsmash = import ../vapoursynth-lsmash.nix;
-  vapoursynth-fft3dfilter = import ../vapoursynth-fft3dfilter.nix;
-  vapoursynth-fmtconv = import ../vapoursynth-fmtconv.nix;
-  vapoursynth-nnedi3 = import ../vapoursynth-nnedi3.nix;
-  vapoursynth-bm3d = import ../vapoursynth-bm3d.nix;
-  knlmeanscl = import ../knlmeanscl.nix;
+  lsmash = import ./lsmash.nix;
+  vapoursynth-lsmash = import ./vapoursynth-lsmash.nix;
+  vapoursynth-fft3dfilter = import ./vapoursynth-fft3dfilter.nix;
+  vapoursynth-fmtconv = import ./vapoursynth-fmtconv.nix;
+  vapoursynth-nnedi3 = import ./vapoursynth-nnedi3.nix;
+  vapoursynth-bm3d = import ./vapoursynth-bm3d.nix;
+  knlmeanscl = import ./knlmeanscl.nix;
 }
